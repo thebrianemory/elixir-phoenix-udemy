222a98d0186d8dedd275f1ea4114cef4b752c513
thebrianemory <the@brianemory.com>
2017-04-06 19:11:07 -0700

    End of video 18

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,13 +1,27 @@
 defmodule Cards do
   def create_deck do
-    ["Ace", "Two", "Three"]
+    values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
+    end
   end
 
   def shuffle(deck) do
-
+    Enum.shuffle(deck)
   end
 
-  def shuffle(deck, table) do
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
 
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
+  end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
   end
 end
