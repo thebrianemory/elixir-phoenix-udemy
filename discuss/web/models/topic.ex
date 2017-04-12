3e45e64e63d07f04b7618e143e4ba237e8065c1c
thebrianemory <the@brianemory.com>
2017-04-11 21:29:32 -0700

    End of video 72

--- discuss/web/models/topic.ex
+++ discuss/web/models/topic.ex
@@ -8,6 +8,6 @@ defmodule Discuss.Topic do
   def changeset(struct, params \\ %{}) do
     struct
     |> cast(params, [:title])
-    |> validate_require([:title])
+    |> validate_required([:title])
   end
 end
